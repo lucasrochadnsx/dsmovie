c8ee916e0c3a442163d66e86de90d96b190aaede
Lucas Rocha <lucasrochadnsx@gmail.com>
2026-04-02 23:43:51 -0300

    Correcao

--- backend/src/main/java/br/com/mrlrsx/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/br/com/mrlrsx/dsmovie/config/SecurityConfig.java
@@ -29,10 +29,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://69cf280d5fece30008a66c50--zingy-mousse-aa9301.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
